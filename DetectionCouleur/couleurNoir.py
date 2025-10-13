ac0a4728c88b7aea7eec012f552a85fd1a2c95ab
lyane-l <lyaneprecieuselegernard@gmail.com>
2025-10-13 13:59:06 +0200

    Update couleurNoir.py

--- DetectionCouleur/couleurNoir.py
+++ DetectionCouleur/couleurNoir.py
@@ -1,42 +1,6 @@
 import numpy as np 
 import cv2 
 
-#------------------Image noire--------------------------------------------------------------------------
-# lire l'image 
-image = cv2.imread("DetectionCouleur/images/boule.jpg",-1)
- #mettre le filtre 
-image_hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-#mettre le masque 
-masked= cv2.inRange(image_hsv,np.array([0, 0, 0]),np.array([180, 255, 100]))
-
-result_black= cv2.bitwise_and(image, image, mask=masked)
-
-
-#afficher l'image
-#cv2.imshow('imageHSV', result_black)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
-#---------------------------image blanche ---------------------------------------------------------
-# lire l'image 
-image1 = cv2.imread("DetectionCouleur/images/les_amies.webp",-1)
- #mettre le filtre 
-image_hsv1 = cv2.cvtColor(image1,cv2.COLOR_BGR2HSV)
-#mettre le masque 
-masked1= cv2.inRange(image_hsv1,np.array([0, 0, 200]),np.array([180, 30, 255]))
-
-result_black1= cv2.bitwise_and(image1, image1, mask=masked1)
-
-
-#afficher l'image
-#cv2.imshow('imageHSV', result_black1)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
-#----------------------------------------------Acces camera ---------------------------------------------
-
-# Test de la caméra avec opencv pour la detection de couleur 
-
 # Ouvrir la webcam (0 = webcam par défaut, 1 = autre caméra si branchée)
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
