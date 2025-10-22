e6085ee13088604a593e6dbfd4d8704da214a709
Zyko <modaherry@gmail.com>
2025-10-22 07:51:27 +0000

    fix (s501) : ajout du code de désabonnement à un service

--- scripts/utils/subricber.py
+++ scripts/utils/subricber.py
@@ -1,12 +1,12 @@
 """
-Module permettant de s'abonner à un événement dans le robot
+Module permettant de s'abonner et se désaboonner à un événement dans le robot
 """
 
 from typing import Any, Callable
 
-def Subriber(session : Any, onEvent : Callable[[Any], None], eventName : str) -> None:
+def Subscriber(session : Any, onEvent : Callable[[Any], None], eventName : str) -> None:
     """
-    S'abonne à l'événement eventName du robot
+    S'abonne à l'événement `eventName` du robot
     
     Args:
         session: Session en cours avec le robot
@@ -16,5 +16,17 @@ def Subriber(session : Any, onEvent : Callable[[Any], None], eventName : str) ->
     memoire = session.service("ALMemory")
     subriber = memoire.subscriber(eventName)
     subriber.signal.connect(onEvent) # connect ne reverra rien, il faudra se débrouiller autremen si vous voulez réutiliser la valariable ailleurs
+
+def UnSubscriber(session : Any, enventName : str) -> None:
+    """
+    Se désabonne à l'évènement `eventName` du robot
+    
+    Args:
+        session: Session en cours avec le robot
+        enventName: nom de l'évènement à se désabonner
+    """
+    
+    memoire = session.service("ALMemory")
+    memoire.unsubcriber(enventName)
 
 if __name__ == '__main__' : pass
