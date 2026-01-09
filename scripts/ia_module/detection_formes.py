a09a9451caa6ee0a366ba1520227627449210414
Ludivine L <levallois.ludivine@gmail.com>
2026-01-09 14:04:48 +0100

    feat(s501) : adapt code for NaO instead of using pc webcam

--- scripts/ia_module/detection_formes.py
+++ scripts/ia_module/detection_formes.py
@@ -5,31 +5,16 @@ from ultralytics import YOLO
 
 MODEL_FILENAME = "deepfashion2_yolov8s-seg.pt"
 
-def lancer_segmentation():
+def detection_formes(frame):
     model = YOLO(MODEL_FILENAME)
 
-    cap = cv2.VideoCapture(0)
+    results = model.predict(source=frame, conf=0.4, stream=True, verbose=False)
 
-    while True:
-        ret, frame = cap.read()
-        if not ret: break
+    for r in results:
+        annotated_frame = r.plot()
 
-        results = model.predict(source=frame, conf=0.4, stream=True, verbose=False)
+        for box in r.boxes:
+            label = model.names[int(box.cls[0])]
+            print(f"NAO voit précisément : {label}")
 
-        for r in results:
-            annotated_frame = r.plot()
-
-            for box in r.boxes:
-                label = model.names[int(box.cls[0])]
-                print(f"NAO voit précisément : {label}")
-
-        cv2.imshow("IA Segmentation Fashion", annotated_frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    lancer_segmentation()
+    return annotated_frame
