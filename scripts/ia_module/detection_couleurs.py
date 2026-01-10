16732e4ff76686c41b2ab5133545e71415e25e00
Ludivine Levallois <levallois.ludivine@gmail.com>
2026-01-10 17:38:35 +0100

    feat(s501): link color detection to clothes detection

--- scripts/ia_module/detection_couleurs.py
+++ scripts/ia_module/detection_couleurs.py
@@ -2,38 +2,30 @@ import cv2
 import numpy as np
 import pandas as pd
 
+COLORS_DF = pd.read_csv("scripts/ia_module/couleurs.csv")
 
-def detection_couleurs () :
-    # Charger le fichier CSV avec les plages de couleurs
-    colors_df = pd.read_csv("scripts/ia_module/couleurs.csv")
+def detection_couleurs(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, valid_pixels_mask = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    meilleur_score = 0
+    couleur_dominante = "Aucune"
+
+    for _, row in COLORS_DF.iterrows():
+        lower = np.array([row['h_min'], row['s_min'], row['v_min']])
+        upper = np.array([row['h_max'], row['s_max'], row['v_max']])
+        
+        color_mask = cv2.inRange(hsv, lower, upper)
+        
+        final_mask = cv2.bitwise_and(color_mask, valid_pixels_mask)
+
+        nb_pixels = cv2.countNonZero(final_mask)
+
+        if nb_pixels > meilleur_score and nb_pixels > 300:
+            meilleur_score = nb_pixels
+            couleur_dominante = row['nom']
     
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-    
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        detected_colors = []
-    
-        for _, row in colors_df.iterrows():
-            lower = np.array([row['h_min'], row['s_min'], row['v_min']])
-            upper = np.array([row['h_max'], row['s_max'], row['v_max']])
-            mask = cv2.inRange(hsv, lower, upper)
-    
-            if cv2.countNonZero(mask) > 500:  # seuil pour éviter le bruit
-                detected_colors.append(row['nom'])
-    
-        # Affichage du texte sur l’image
-        text = " | ".join(set(detected_colors)) if detected_colors else "Aucune couleur détectée"
-        cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-    
-        cv2.imshow("Detection de couleurs", frame)
-    
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-    
-    cap.release()
-    cv2.destroyAllWindows()
-    
+    return couleur_dominante
--- scripts/ia_module/detection_formes.py
+++ scripts/ia_module/detection_formes.py
@@ -1,19 +1,69 @@
+import cv2
 import os
+import time
+import numpy as np
 from ultralytics import YOLO
+from detection_couleurs import detection_couleurs
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_FILENAME = os.path.join(SCRIPT_DIR, "deepfashion2_yolov8s-seg.pt")
+model = YOLO(MODEL_FILENAME)
 
-def detection_formes(frame):
-    model = YOLO(MODEL_FILENAME)
+def extraire_segment(frame, mask, box):
 
-    results = model.predict(source=frame, conf=0.4, stream=True, verbose=False)
+    mask_data = mask.data[0].cpu().numpy()
+    mask_binary = cv2.resize(mask_data, (frame.shape[1], frame.shape[0]))
+    mask_binary = (mask_binary > 0.5).astype(np.uint8) * 255
 
-    for r in results:
-        annotated_frame = r.plot()
+    isolated_object = cv2.bitwise_and(frame, frame, mask=mask_binary)
 
-        for box in r.boxes:
-            label = model.names[int(box.cls[0])]
-            print(f"NAO voit précisément : {label}")
+    x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+    crop_detoure = isolated_object[y1:y2, x1:x2]
+    
+    return crop_detoure
 
-    return annotated_frame
+def detection_formes():
+    cap = cv2.VideoCapture(0)
+    
+    dernier_temps_print = 0 
+
+    while True:
+        ret, frame = cap.read()
+        if not ret: break
+
+        results = model.predict(source=frame, conf=0.4, verbose=False)
+        
+        infos_a_afficher = []
+
+        for r in results:
+            annotated_frame = r.plot() 
+
+            if r.masks is not None:
+                for i, (mask, box) in enumerate(zip(r.masks, r.boxes)):
+                    label = model.names[int(box.cls[0])]
+                    obj_id = f"{label}_{i}"
+                    
+                    vêtement_seul = extraire_segment(frame, mask, box)
+
+                    if vêtement_seul.size > 0:
+                        couleur = detection_couleurs(vêtement_seul)
+                        infos_a_afficher.append(f"{label}: {couleur}")
+
+        temps_actuel = time.time()
+        
+        if temps_actuel - dernier_temps_print >= 1.0:
+            if infos_a_afficher:
+                print(f" | ".join(infos_a_afficher))
+            
+            dernier_temps_print = temps_actuel
+
+        cv2.imshow("IA Segmentation Fashion", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    detection_formes()
