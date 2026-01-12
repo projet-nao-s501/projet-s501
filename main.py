c6a4f2e8dfb767e91b5b55086f9d202ef885b101
Islem Foudil <islem.foudil@universite-paris-saclay.fr>
2026-01-12 17:33:37 +0100

    feat(s501): call func checker in main

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import qi
 import argparse
 import sys
 import time
-from scripts.meca_module.reaction_nao import naoDab, naoDanse, sad
+from scripts.meca_module.reaction_nao import naoDab, naoDanse, sad, checker
 
 def main(session, args) :
-    sad(session)
+    checker(session)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Contrôle du robot NAO.")
