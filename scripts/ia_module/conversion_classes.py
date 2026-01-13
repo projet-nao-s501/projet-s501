f5b95f7dc25ab88fc72961573301ade70a94b3e5
Islem Foudil <islem.foudil@universite-paris-saclay.fr>
2026-01-13 11:59:56 +0100

    feat(s501): add tab value-key to convert what we say and what the robot detect (french-english) with more common word used - easy to compare after

---
+++ scripts/ia_module/conversion_classes.py
@@ -0,0 +1,15 @@
+vetements = {
+	"pull": "long sleeve shirt",
+	"tshirt": "short_sleeved_shirt",
+	"veste manche courte": "short_sleeved_outwear",
+	"veste manche longue": "long_sleeved_outwear",
+	"gilet": "vest",
+	"sacoche": "sling",
+	"short": "shorts",
+	"pantalon": "trousers",
+	"jupe": "skirt",
+	"robe manche courte": "short_sleeved_dress",
+	"robe manche longue": "long_sleeved_dress",
+	"robe tailleur": "vest_dress",
+	"robe bretelle": "sling_dress"
+}
